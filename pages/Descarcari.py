fd58683b5dd0d09389916f9fc2cfb988ee3fc835
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-25 00:03:05 +0200

    Update Descarcari.py

--- pages/Descarcari.py
+++ pages/Descarcari.py
@@ -1,19 +1,19 @@
 import streamlit as st
 
 
-@st.cache
-def convert_df(df):
+#@st.cache
+#def convert_df(df):
     # IMPORTANT: Cache the conversion to prevent computation on every rerun
-    return df.to_csv().encode('utf-8')
+ #   return df.to_csv().encode('utf-8')
 
-csv = convert_df(my_large_df)
+#csv = convert_df(my_large_df)
 
-st.download_button(
-    label="Download data as CSV",
-    data=csv,
-    file_name='large_df.csv',
-    mime='text/csv',
-)
+#st.download_button(
+#    label="Download data as CSV",
+#    data=csv,
+#    file_name='large_df.csv',
+#    mime='text/csv',
+#)
 
 text_contents = '''This is some text'''
 st.download_button('Download text', text_contents)
