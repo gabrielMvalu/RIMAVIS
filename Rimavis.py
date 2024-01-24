673a8d69936dbbce6aff494b8bd37df91bb768fd
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-24 22:43:52 +0200

    Update Rimavis.py

--- Rimavis.py
+++ Rimavis.py
@@ -1,39 +1,15 @@
 # app.py
 import streamlit as st
-from PIL import Image
+from streamlit import config as _config
 
-# Setarea configurației paginii trebuie să fie prima instrucțiune Streamlit
+# Asigurați-vă că titlul paginii este setat conform preferințelor dvs.
 st.set_page_config(page_title='Aplicația SEAP', page_icon=None, layout='centered', initial_sidebar_state='auto')
 
-# Funcția main va rula logica principală a aplicației
-def main():
-    st.markdown("""
-        <style>
-        @import url('https://fonts.googleapis.com/css2?family=Patrick+Hand&display=swap');
-        .title {
-            color: #7FBBE9; /* A modern shade of blue */
-            font-family: 'Comic Sans MS', cursive, sans-serif; /* Comic Sans MS with fallbacks */
-            font-size: 30px; /* Adjust the size as needed */
-            font-weight: 700; /* 700 is for bold text */
-            text-align: center; /* Center align for modern aesthetics */
-            margin-bottom: 20px; /* Add some space below the title */
-        }
-        </style>
-    
-        <h1 class='title'>Pregatirea datelor in vederea intocmirii documentatiei pentru 'Licitatii SEAP'</h1>
-        """, unsafe_allow_html=True)
-  
-    # Sidebar pentru încărcarea și afișarea logo-ului și textului
-    st.sidebar.title("Încărcarea Documentelor")
-    logo_path = "LogoSTR.PNG"
-    try:
-        logo = Image.open(logo_path)
-        st.sidebar.image(logo, use_column_width=True)
-    except IOError:
-        st.sidebar.error("Eroare la încărcarea logo-ului.")
+# Puteți adăuga un logo și un titlu în bara laterală dacă doriți
+st.sidebar.image('logo.png', use_column_width=True)
+st.sidebar.title('Navigare')
 
-    st.sidebar.markdown("<small>© Castemill S.R.L.</small>", unsafe_allow_html=True)
-
-# Apelarea funcției main
-if __name__ == "__main__":
-    main()
+# Restul logicii paginii principale poate fi adăugată aici dacă este necesar
+# Sidebar       
+with st.sidebar:
+    st.image("./assets/Rimavis.png",   use_column_width=True)
