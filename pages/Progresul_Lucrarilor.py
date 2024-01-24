b23abf37037c4a9e0701110551de0760a672b57d
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-24 23:52:45 +0200

    Update Progresul_Lucrarilor.py

--- pages/Progresul_Lucrarilor.py
+++ pages/Progresul_Lucrarilor.py
@@ -9,8 +9,8 @@ def run():
     progress_text = "Operation in progress. Please wait."
     my_bar = st.progress(0, text=progress_text)
     
-    for percent_complete in range(1000):
-        time.sleep(0.01)
+    for percent_complete in range(100):
+        time.sleep(0.5)
         my_bar.progress(percent_complete + 1, text=progress_text)
     time.sleep(1)
     my_bar.empty()
