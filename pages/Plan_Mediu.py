3a4853ae43ed92ec040b2d3627a0ae6fec691942
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-24 23:04:23 +0200

    Update Plan_Mediu.py

--- pages/Plan_Mediu.py
+++ pages/Plan_Mediu.py
@@ -5,5 +5,9 @@ def run():
     st.title('Planul de Mediu')
     st.write('Aceasta este pagina despre Planul de Mediu')
 
+# Puteți adăuga un logo și un titlu în bara laterală dacă doriți
+st.sidebar.image('./assets/rimavis.PNG', use_column_width=True)
+st.sidebar.title('Generarea PTE-uri necesare PM')
+
 if __name__ == '__main__':
     run()
