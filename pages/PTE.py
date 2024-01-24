35bfbcae4e1989fd00f173c9a5a3598c93c1bcc6
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-24 22:11:26 +0200

    Create PTE.py

---
+++ pages/PTE.py
@@ -0,0 +1,9 @@
+# pages/About.py
+import streamlit as st
+
+def run():
+    st.title('Plan tehnic de executie')
+    st.write('Aceasta este pagina despre aplicația noastră pentru licitații pe SEAP.')
+
+if __name__ == '__main__':
+    run()
