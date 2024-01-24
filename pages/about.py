3a30cc8412a457515c46e174a67b4303f5430f32
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-24 22:14:30 +0200

    Update about.py

--- pages/about.py
+++ pages/about.py
@@ -2,7 +2,7 @@
 import streamlit as st
 
 def run():
-    st.title('Despre Aplicație')
+    st.title('Genereaza PTE')
     st.write('Aceasta este pagina despre aplicația noastră pentru licitații pe SEAP.')
 
 if __name__ == '__main__':
