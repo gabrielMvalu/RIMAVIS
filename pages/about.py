8d72bb0aded5f140b3c0152f50a91e307ff3ccff
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-24 22:57:55 +0200

    Delete pages/about.py

--- pages/about.py
+++
@@ -1,9 +0,0 @@
-# pages/About.py
-import streamlit as st
-
-def run():
-    st.title('Genereaza PTE')
-    st.write('Aceasta este pagina despre aplicația noastră pentru licitații pe SEAP.')
-
-if __name__ == '__main__':
-    run()
