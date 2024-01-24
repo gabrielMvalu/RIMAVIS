9638f54bafcf61bcb197bb8f20c333925b2b0702
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-24 23:11:07 +0200

    Update PTE.py

--- pages/PTE.py
+++ pages/PTE.py
@@ -3,16 +3,20 @@ import streamlit as st
 
 def run():
     st.title('Plan tehnic de executie')
-    st.write('Aceasta este pagina despre aplicația noastră pentru licitații pe SEAP.')
+    st.write('Selectati PTE nececsar pentru licitatia in curs.')
+
+    agree = st.checkbox('I agree')
+    
+    if agree:
+        st.write('Great!')
+
+
 
 # Puteți adăuga un logo și un titlu în bara laterală dacă doriți
 st.sidebar.image('./assets/rimavis.PNG', use_column_width=True)
 st.sidebar.title('Generarea PTE-uri necesare PM')
 
-agree = st.checkbox('I agree')
 
-if agree:
-    st.write('Great!')
 
 
 if __name__ == '__main__':
