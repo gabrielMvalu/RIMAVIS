4f1e7699d281c4f928e79a3223ecd89390f12482
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-24 23:03:16 +0200

    Create Plan_Mediu.py

---
+++ pages/Plan_Mediu.py
@@ -0,0 +1,9 @@
+# pages/PlanMediu.py
+import streamlit as st
+
+def run():
+    st.title('Planul de Mediu')
+    st.write('Aceasta este pagina despre Planul de Mediu')
+
+if __name__ == '__main__':
+    run()
