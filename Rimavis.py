dc4b22bf4cfa326e0a59b2c1ac030cacf2b2350f
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-24 22:56:06 +0200

    Update Rimavis.py

--- Rimavis.py
+++ Rimavis.py
@@ -10,6 +10,21 @@ st.sidebar.image('./assets/rimavis.PNG', use_column_width=True)
 st.sidebar.title('Navigare')
 
 
+st.markdown("""
+       <style>
+       @import url('https://fonts.googleapis.com/css2?family=Patrick+Hand&display=swap');
+       .title {
+           color: #7FBBE9; /* A modern shade of blue */
+           font-family: 'Comic Sans MS', cursive, sans-serif; /* Comic Sans MS with fallbacks */
+           font-size: 30px; /* Adjust the size as needed */
+           font-weight: 700; /* 700 is for bold text */
+           text-align: center; /* Center align for modern aesthetics */
+           margin-bottom: 20px; /* Add some space below the title */
+       }
+       </style>
+  
+       <h1 class='title'>Pregatirea documentatiei 'Ofertant' - licitatii SEAP</h1>
+       """, unsafe_allow_html=True)
 
 # Restul logicii paginii principale poate fi adăugată aici dacă este necesar
 # Sidebar       
