b1f94e68669911c745a34a28332de81d1e85abf1
gabrielMvalu <47900852+gabrielMvalu@users.noreply.github.com>
2024-01-24 22:57:38 +0200

    Update PTE.py

--- pages/PTE.py
+++ pages/PTE.py
@@ -5,5 +5,9 @@ def run():
     st.title('Plan tehnic de executie')
     st.write('Aceasta este pagina despre aplicația noastră pentru licitații pe SEAP.')
 
+# Puteți adăuga un logo și un titlu în bara laterală dacă doriți
+st.sidebar.image('./assets/rimavis.PNG', use_column_width=True)
+st.sidebar.title('Generarea PTE-uri necesare PM')
+
 if __name__ == '__main__':
     run()
